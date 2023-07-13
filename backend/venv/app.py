0bd37ab06bdca935e4ec42212b76f995cb1db402
Abdurrahman Olalere <112133928+LosingLogic@users.noreply.github.com>
2023-07-13 13:49:40 -0500

    i fixed the image stuff, still a bit laggy loop

--- backend/venv/app.py
+++ backend/venv/app.py
@@ -1,10 +1,9 @@
 import db as db
 import mail as mail
 import os
-from flask import Flask,render_template,request,url_for,jsonify
+from flask import Flask, render_template, request, url_for, jsonify
 import json
-# opencv doesn't seem to support gif so i'm trying imageio
-
+import imageio
 
 app = Flask(__name__)
 
@@ -20,13 +19,14 @@ def contact():
         url_for('static', filename=f'img/swords/{image_file}')
         for image_file in image_files
     ]
+    # print(image_files_urls) use to check image urls
 
     # Convert the image file URLs to a regular list of strings
     image_files_list = [str(url) for url in image_files_urls]
 
     # Convert the image files list to JSON
-    image_files_json = jsonify(image_files_list)
-    return render_template('contact.html',image_files_json=image_files_json)
+    image_files_json = json.dumps(image_files_list)
+    return render_template('contact.html', image_files_json=image_files_json)
 
 @app.route('/')
 def index():
@@ -45,28 +45,29 @@ def index():
     image_files_list = [str(url) for url in image_files_urls]
 
     # Convert the image files list to JSON
-    image_files_json = jsonify(image_files_list)
+    image_files_json = json.dumps(image_files_list)
     return render_template('index.html', image_files_json=image_files_json)
 
 
 
-# @app.route('/get_images', methods=['GET'])
-# def get_images():
-#     image_folder = os.path.join(app.static_folder, 'img','swords')  # Folder path where images are stored
-    
-#     # Get the list of image file names in the folder
-#     image_files = os.listdir(image_folder)
-    
-#     frames = []
-#     for filename in image_files:
-#         image_path = os.path.join(image_folder, filename)
-#         frame = cv2.imread(image_path)
-#         frames.append(frame)
-        
-#     return jsonify({'images': frames})
+@app.route('/get_images')
+def get_images():
+    images_folder = os.path.join(app.static_folder, 'img', 'swords')
+
+    # Get the list of image file names in the folder
+    image_files = sorted(os.listdir(images_folder))
+
+    # Retrieve the image file URLs
+    image_files_urls = [
+        url_for('static', filename=f'img/swords/{image_file}')
+        for image_file in image_files
+    ]
+
+    return jsonify({'image_urls': image_files_urls})
 
 
-""" @app.route("/generate_gif", methods=["POST"])
+# this route is giving me some trouble, the gif isn't being displaye i'm so dumb maybe nowthis works becuaase the script is called after the element exists.
+@app.route("/generate_gif", methods=["POST"])
 def generate_gif():
     image_folder = os.path.join(app.static_folder, 'img','swords')
 
@@ -82,7 +83,7 @@ def generate_gif():
     imageio.mimsave(gif_path, frames, duration=0.2)
 
     return jsonify({"gif_url": gif_path})
- """
+ 
 
 @app.route('/insert', methods = ['POST'])
 def insert():
--- backend/venv/static/js/script.js
+++ backend/venv/static/js/script.js
@@ -1,69 +1,55 @@
-// Get the canvas element
-const canvas = document.getElementById('gif-canvas');
-const ctx = canvas.getContext('2d');
+const container = document.querySelector('.animation-container');
 
-// Function to set the canvas size based on the screen size
-function setCanvasSize() {
-    const desiredWidth = window.innerWidth * 0.75;
-    const desiredHeight = window.innerHeight * 0.75;
+// Function to display images as animation
+function displayAnimation(imageUrls) {
+let currentIndex = 0;
 
-    canvas.width = desiredWidth;
-    canvas.height = desiredHeight;
+function showImage() {
+    const image = new Image();
+    image.onload = function() {
+        const animatedImage = document.createElement('img');
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+        const imgHeight = screenHeight * 1;
+        const imgWidth = screenWidth;
 
-    // Start displaying the GIF
-    displayGif();
+        animatedImage.src = image.src;
+        animatedImage.id = 'animated-image'; // Set the id attribute
+
+        // Styling the img for proper scale
+        animatedImage.style.height = `${imgHeight}px`;
+        animatedImage.style.width = `${imgWidth}px`;
+
+        // Check if container exists before setting innerHTML
+        if (container != null) {
+        container.innerHTML = '';
+        container.appendChild(animatedImage);
+        
+        }
+        
+        
+    };
+    image.src = imageUrls[currentIndex];
+
+    currentIndex++;
+    if (currentIndex >= imageUrls.length) {
+    currentIndex = 0;
+    }
+
+    setTimeout(showImage, 200); // Adjust the delay as per your requirements
 }
 
-// Function to display the images as a GIF
-function displayGif() {
-    let currentIndex = 0;
-  
-    function render() {
-      const image = new Image();
-      image.onload = function() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(image, 0, 0);
-      };
-      image.src = imageFiles[currentIndex];
-  
-      currentIndex++;
-      if (currentIndex >= imageFiles.length) {
-        currentIndex = 0;
-      }
-  
-      setTimeout(render, 200); // Adjust the delay as per your requirements
-    }
-  
-    render();
-  }
-  
+showImage();
+}
 
-// Call the setCanvasSize function initially
-setCanvasSize();
+// Make a GET request to retrieve the image URLs
+fetch('/get_images')
+.then(response => response.json())
+.then(data => {
+    const imageUrls = data.image_urls;
+    displayAnimation(imageUrls);
+})
+.catch(error => console.log(error));
 
-// Handle window resize events
-window.addEventListener('resize', setCanvasSize);
 
-  
-  
-  
 
-// Make a request to the Flask server to generate the GIF
-/* fetch("/generate_gif", {
-    method: "POST",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const gifUrl = data.gif_url;
-  
-      // Create an image element to display the generated GIF
-      const gifImg = document.createElement("img");
-      gifImg.src = gifUrl;
-  
-      // Append the image to the animation container
-      const animationContainer = document.getElementById("animation-container");
-      animationContainer.appendChild(gifImg);
-    });
-    // this isn't working  so i'l comment it out becuase ikeep getting 404 errors for this
-   */
-  
